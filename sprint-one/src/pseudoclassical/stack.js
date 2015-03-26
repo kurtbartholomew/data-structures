0c0821ae10446d1c12845bd0ac0c88ed68f0a8c2
kurtbartholomew <siever246@gmail.com>
2015-03-25 20:29:57 -0700

    Completed all of the challenges.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
+
+Stack.prototype.push = function( value ) {
+  this.storage[this.index]=value;
+  this.index++;
+};
+
+Stack.prototype.pop = function( ) {
+  if(this.size()>0){
+    this.index--;
+    return this.storage[this.index];
+  }
+};
+
+Stack.prototype.size = function( ) {
+  return this.index;
+};
+
 
 
