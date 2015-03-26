4915e2b2e8224fa3ff1b177b2b033c88a86b25c3
kurtbartholomew <siever246@gmail.com>
2015-03-26 14:00:36 -0700

    Finished hasEdge() of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,26 +1,40 @@
 
 
 var Graph = function(){
+  this.storage = [];
 };
 
-Graph.prototype.addNode = function(node){
-
+Graph.prototype.addNode = function(value){
+  this.storage.push(graphNode(value));
 };
 
-Graph.prototype.contains = function(node){
-
+Graph.prototype.getNode = function( value ) {
+  return this.storage[this.getIndexOf( value )];
 };
 
-Graph.prototype.removeNode = function(node){
-
+Graph.prototype.getIndexOf = function( value ) {//TODO: Use indexOf instead?
+  for(var index in this.storage) {
+    if(this.storage[index].value === value) {
+      return index;
+    }
+  }
+  return undefined;
 };
 
-Graph.prototype.hasEdge = function(fromNode, toNode){
-
+Graph.prototype.contains = function( value ) {
+  return !!this.getNode( value );
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
+Graph.prototype.removeNode = function( value ) {
+  delete this.storage[this.getIndexOf( value )];
+};
 
+Graph.prototype.hasEdge = function(fromNodeValue, toNodeValue){
+  return this.getNode(fromNodeValue).edges.indexOf(toNodeValue) > -1;
+};
+
+Graph.prototype.addEdge = function(fromNodeValue, toNodeValue){
+  this.getNode(fromNodeValue).edges.push(toNodeValue);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
@@ -31,6 +45,12 @@ Graph.prototype.forEachNode = function(cb){
 
 };
 
+var graphNode = function( value ){
+  nodeInstance = {};
+  nodeInstance.value = value;
+  nodeInstance.edges = [];
+  return nodeInstance;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  * addNode :
