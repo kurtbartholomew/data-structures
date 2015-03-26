c9659361ea3ab3b60af531a313da411681317dab
kurtbartholomew <siever246@gmail.com>
2015-03-26 14:19:05 -0700

    Added Graph.Remove Edge.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -35,10 +35,18 @@ Graph.prototype.hasEdge = function(fromNodeValue, toNodeValue){
 
 Graph.prototype.addEdge = function(fromNodeValue, toNodeValue){
   this.getNode(fromNodeValue).edges.push(toNodeValue);
+  if(this.getNode(toNodeValue).edges.indexOf(fromNodeValue)===-1){
+    this.addEdge(toNodeValue, fromNodeValue);
+  }
 };
 
-Graph.prototype.removeEdge = function(fromNode, toNode){
-
+Graph.prototype.removeEdge = function(fromNodeValue, toNodeValue){
+  var edge=this.getNode(fromNodeValue).edges[toNodeValue];
+  if(edge!==undefined){
+    delete this.getNode(fromNodeValue).edges[toNodeValue];
+    this.removeEdge(toNodeValue,fromNodeValue);
+  }
+  //this.getNode(fromNodeValue).edges.indexOf(toNodeValue)
 };
 
 Graph.prototype.forEachNode = function(cb){
@@ -46,7 +54,7 @@ Graph.prototype.forEachNode = function(cb){
 };
 
 var graphNode = function( value ){
-  nodeInstance = {};
+  var nodeInstance = {};
   nodeInstance.value = value;
   nodeInstance.edges = [];
   return nodeInstance;
